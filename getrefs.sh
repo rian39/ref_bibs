5f3187ed7bb132c73f5aab7a4ad3fb2385c8e491
rian39 <a.mackenzie@lancaster.ac.uk>
2015-03-01 21:23:11 +0000

    getrefs.sh

---
+++ getrefs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget http://localhost:23119/better-bibtex/collection?/0/5BFPRVNK.biblatex -O ~/Documents/ref_bibs/data_forms_thought.bib
+wget http://localhost:23119/better-bibtex/collection?/0/63WRVW6I.biblatex -O ~/Documents/ref_bibs/google_analytics.bib
+wget http://localhost:23119/better-bibtex/collection\?/0/944237BV.biblatex -O ~/Documents/ref_bibs/mackenzie.bib 
+wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/Documents/ref_bibs/ngs.bib
