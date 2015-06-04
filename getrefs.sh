78fa6d8346835943e3c8f7e00146f743a4238d29
rian39 <a.mackenzie@lancaster.ac.uk>
2015-06-04 11:30:22 +0100

    recent update

--- getrefs.sh
+++ getrefs.sh
@@ -3,4 +3,5 @@ wget http://localhost:23119/better-bibtex/collection?/0/5BFPRVNK.biblatex -O ~/D
 wget http://localhost:23119/better-bibtex/collection?/0/63WRVW6I.biblatex -O ~/Documents/ref_bibs/google_analytics.bib
 wget http://localhost:23119/better-bibtex/collection\?/0/944237BV.biblatex -O ~/Documents/ref_bibs/mackenzie.bib 
 wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/Documents/ref_bibs/ngs.bib
+wget http://localhost:23119/better-bibtex/collection?/0/TUQPFESX.biblatex O ~/Documents/ref_bibs/at_this_moment.bib
 
