8d3cf6e86f23704e5f77fd03c89f9cf1459e725c
abm <a.mackenzie@lancaster.ac.uk>
2016-01-25 11:18:07 +0000

    recent update

--- getrefs.sh
+++ getrefs.sh
@@ -4,4 +4,5 @@ wget http://localhost:23119/better-bibtex/collection?/0/63WRVW6I.biblatex -O ~/D
 wget http://localhost:23119/better-bibtex/collection\?/0/944237BV.biblatex -O ~/Documents/ref_bibs/mackenzie.bib 
 wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/Documents/ref_bibs/ngs.bib
 wget http://localhost:23119/better-bibtex/collection?/0/TUQPFESX.biblatex -O ~/Documents/ref_bibs/at_this_moment.bib
+wget http://localhost:23119/better-bibtex/collection?/0/A6NDHZ4R.biblatex -O ~/Documents/ref_bibs/metacommunities.bib
 
