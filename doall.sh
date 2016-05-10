5dba0a7e58823c94d316b663c3460ae81cc14891
abm <a.mackenzie@lancaster.ac.uk>
2016-05-10 11:15:12 +0100

    recent update

--- doall.sh
+++ doall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ./getrefs.sh
+./combine_bib.sh
 ./update.sh
 
--- getrefs.sh
+++ getrefs.sh
@@ -5,4 +5,5 @@ wget http://localhost:23119/better-bibtex/collection\?/0/944237BV.biblatex -O ~/
 wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/Documents/ref_bibs/ngs.bib
 wget http://localhost:23119/better-bibtex/collection?/0/TUQPFESX.biblatex -O ~/Documents/ref_bibs/at_this_moment.bib
 wget http://localhost:23119/better-bibtex/collection?/0/A6NDHZ4R.biblatex -O ~/Documents/ref_bibs/metacommunities.bib
+wget http://localhost:23119/better-bibtex/collection?/0/R.biblatex -O ~/Documents/ref_bibs/R.bib
 
