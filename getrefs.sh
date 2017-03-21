0a39899372fd881175f0846aa12cb9fe66d3af70
abm <a.mackenzie@lancaster.ac.uk>
2017-03-21 09:28:30 +0000

    recent update

--- getrefs.sh
+++ getrefs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-wget http://localhost:23119/better-bibtex/collection?/0/5BFPRVNK.biblatex -O ~/Documents/ref_bibs/data_forms_thought.bib
-wget http://localhost:23119/better-bibtex/collection?/0/63WRVW6I.biblatex -O ~/Documents/ref_bibs/google_analytics.bib
-wget http://localhost:23119/better-bibtex/collection?/0/mackenzie.biblatex -O ~/Documents/ref_bibs/mackenzie.bib 
-wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/Documents/ref_bibs/ngs.bib
-wget http://localhost:23119/better-bibtex/collection?/0/TUQPFESX.biblatex -O ~/Documents/ref_bibs/at_this_moment.bib
-wget http://localhost:23119/better-bibtex/collection?/0/A6NDHZ4R.biblatex -O ~/Documents/ref_bibs/metacommunities.bib
-wget http://localhost:23119/better-bibtex/collection?/0/R.biblatex -O ~/Documents/ref_bibs/R.bib
-wget http://localhost:23119/better-bibtex/collection?/0/ensembles.biblatex -O ~/Documents/ref_bibs/ensembles.bib
+wget http://localhost:23119/better-bibtex/collection?/0/5BFPRVNK.biblatex -O ~/ref_bibs/data_forms_thought.bib
+wget http://localhost:23119/better-bibtex/collection?/0/63WRVW6I.biblatex -O ~/ref_bibs/google_analytics.bib
+wget http://localhost:23119/better-bibtex/collection?/0/mackenzie.biblatex -O ~/ref_bibs/mackenzie.bib 
+wget http://localhost:23119/better-bibtex/collection?/0/H4X4NHKQ.biblatex -O ~/ref_bibs/ngs.bib
+wget http://localhost:23119/better-bibtex/collection?/0/TUQPFESX.biblatex -O ~/ref_bibs/at_this_moment.bib
+wget http://localhost:23119/better-bibtex/collection?/0/A6NDHZ4R.biblatex -O ~/ref_bibs/metacommunities.bib
+wget http://localhost:23119/better-bibtex/collection?/0/R.biblatex -O ~/ref_bibs/R.bib
+wget http://localhost:23119/better-bibtex/collection?/0/ensembles.biblatex -O ~/ref_bibs/ensembles.bib
 
