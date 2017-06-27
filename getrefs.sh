5f7493cca7a48416d42ed84859d2360643f1d42a
MACKENZA <a.mackenzie@lancaster.ac.uk>
2017-06-27 15:12:25 +0100

    recent update

--- getrefs.sh
+++ getrefs.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 wget http://localhost:23119/better-bibtex/collection?/0/data_forms_thought.biblatex -O ~/ref_bibs/data_forms_thought.bib
-wget http://localhost:23119/better-bibtex/collection?/0/google_analytics.biblatex -O ~/ref_bibs/google_analytics.bib
 wget http://localhost:23119/better-bibtex/collection?/0/mackenzie.biblatex -O ~/ref_bibs/mackenzie.bib 
 wget http://localhost:23119/better-bibtex/collection?/0/ngs.biblatex -O ~/ref_bibs/ngs.bib
 wget http://localhost:23119/better-bibtex/collection?/0/at_this_moment.biblatex -O ~/ref_bibs/at_this_moment.bib
